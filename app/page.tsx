180076bd664cbea93eb6a522ce05aa41fb6d2795
MamatovY <mamatovyiman28@gmail.com>
2023-07-28 21:49:02 +0600

    add img

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import Img from '@/assets/banner3.jpg'
+import NextImage from 'next/image'
+
 export default function Home() {
-  return <h1>Welcome to NextJS world</h1>;
+  return (
+    <div>
+      <h1>
+        Welcome to NextJS world
+      </h1>
+      <div>
+
+        <NextImage
+          alt='Hero Image'
+          src={Img}
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          priority
+        />
+      </div>
+    </div>
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        // deviceSizes:[375,640,750,828,1080,1200,1920]
+    }
+}
 
 module.exports = nextConfig
